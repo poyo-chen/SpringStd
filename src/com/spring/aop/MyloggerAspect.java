3693b94f5ebafd5975cc80083b7ffdbc6276144a
Chen <martinezchen01@icloud.com>
2020-11-02 20:23:48 +0800

    Spring 28

--- src/com/spring/aop/MyloggerAspect.java
+++ src/com/spring/aop/MyloggerAspect.java
@@ -12,7 +12,7 @@ public class MyloggerAspect {
      * @Before:將方法指定為前置通知
      * 必須設置value，其值為切入點表達式
      * */
-    @Before(value = "execution(public int com.spring.aop.MathImpl.add(int,int)")
+    @Before(value = "execution(public int com.spring.aop.MathImpl.add(int,int))")
     public void beforeMethod() {
         System.out.println("方法執行之前");
     }
--- src/com/spring/aop/Test.java
+++ src/com/spring/aop/Test.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext ac=new ClassPathXmlApplicationContext("aop.xml");
-        MathI math=ac.getBean("mathImpl",MathI.class);
+        MathI math=ac.getBean("mathImpl",MathImpl.class);
         System.out.println(math.add(5,1));
     }
 }
