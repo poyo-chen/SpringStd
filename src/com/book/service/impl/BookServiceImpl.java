aa310fccd993a721e7f15a4c6219ee21552d3acf
Chen <martinezchen01@icloud.com>
2020-11-28 11:34:07 +0800

    Spring47-53 Done

--- src/com/book/service/impl/BookServiceImpl.java
+++ src/com/book/service/impl/BookServiceImpl.java
@@ -17,13 +17,19 @@ public class BookServiceImpl implements BookService {
      * @Transactional:對方法中所有操作視為一個事務進行管理
      * 在方法上使用，只對一個方法有效
      * 在類上使用，對類中所有方法有效
-     *propagation:
+     * propagation: A和B方法都有事務，當A在調用B時，會將A中的事物傳播給B方法，B方法對事務的處理方式就是事物的傳播行為
+     * (Propagation.REQUIRED)必須使用調用者的事物
+     * (Propagation.REQUIRED_NEW)不使用調用者的事物，使用新的事物進行處裡
+     * isolation: 事務的隔離級別，在病發的情況下，操作數據的一種規定
+     *      讀未提交 1
+     *      讀已提交 2
+     *      可重複讀 4
+     *      串行化  8
+     * timeout: 在事物強制回滾前最多可以執行(等待)的時間
      *
-     * isolation:
-     *
-     * timeout:
-     *
-     * readonly:
+     * readonly:指定當前事務中的一系列操作是否為只讀
+     * 若設為只讀，mysql就會在請求訪問數去庫時，不加鎖提高性能
+     * 但如果有寫的操作的話不能設置為只讀
      *
      * rollbackFor | rollbackForClassName |noRollbackFor | noRollbackForClassName
      * */
